1d16d9c8d702798313927580dda05dfe6572f29f
rishabh.kamath <rshbhk22@gmail.com>
2022-12-20 19:25:59 -0600

    javascript added

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Generate a random password
+function generatePassword() {
+  var length = 8;
+  var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+
+
+  var password = "";
+
+
+  for (var i = 0; i < length; i++) {
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
